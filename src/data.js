774674c05599b4526e325e3d275ca6c3681001d1
jigooo <h00kk@naver.com>
2023-02-08 16:01:29 +0900

    0208

--- src/data.js
+++ src/data.js
@@ -20,15 +20,6 @@ const RD = [
   },
   {
     id: 2,
-    title: "Youtube Clone",
-    description:
-      "본 프로젝트는 React를 이용하여 인기 동영상, 검색, 동영상 재생 등의 기능을 담은 YouTube 클론 코딩 페이지입니다. Youtube Api를 사용하여 원하는 영상을 검색할 수 있으며, 현재 대한민국 인기 동영상 20개의 리스트를 얻어와 메인화면에 구현하였습니다",
-    tools: " react / css / axios / youtube api ",
-    url: "https://zoseeee.github.io/YouTube/",
-    github: "https://github.com/zoseeee/YouTube",
-  },
-  {
-    id: 3,
     title: "Card Maker",
     description:
       "본 프로젝트는 React를 이용하여, 홈, 로그인, 카드 편집, 등록, 미리보기 등으로 구성되어있습니다. 최근 재밌게 보았던 영화 ‘everything everywhere all at once’ 를 테마로 만든 프로젝트입니다. Firebase를 통해 로그인이 가능하도록 하였고 사용자의 정보를 Firebase의 Realtime Database와 Cloudinary에서 실시간 등록 및 수정이 가능하도록 하였습니다.",
@@ -37,13 +28,23 @@ const RD = [
     github: "https://github.com/zoseeee/Card-Maker",
   },
   {
+    id: 3,
+    title: "Youtube Clone",
+    description:
+      "본 프로젝트는 React를 이용하여 인기 동영상, 검색, 동영상 재생 등의 기능을 담은 YouTube 클론 코딩 페이지입니다. Youtube Api를 사용하여 원하는 영상을 검색할 수 있으며, 현재 대한민국 인기 동영상 20개의 리스트를 얻어와 메인화면에 구현하였습니다",
+    tools: " react / css / axios / youtube api ",
+    url: "https://zoseeee.github.io/YouTube/",
+    github: "https://github.com/zoseeee/YouTube",
+  },
+
+  {
     id: 4,
     title: "Golden Snitch!",
     description:
-      "본 프로젝트는 Javascript로 만든 간단한 게임입니다. 영화 <Harry Potter> 속 퀴티치 경기를 테마로 만들었습니다. 20초의 시간이 주어지고 시간 내에 골든 스니치 10개를 클릭하면 성공하는 아주 간단한 게임입니다. Netlify로 배포 후 수정하고 있습니다. ",
-    tools: " javascript / css / netlify ",
-    url: "https://zoseeee.github.io/portfolio/",
-    github: "",
+      "본 프로젝트는 Javascript로 만든 간단한 미니 게임입니다. 영화 <Harry Potter> 속 퀴티치 경기를 테마로 만들었습니다. 15초의 시간이 주어지고 시간 내에 골든 스니치 10개를 클릭하면 성공하는 아주 간단한 게임입니다. ",
+    tools: " javascript / css  ",
+    url: "https://zoseeee.github.io/mini-game/",
+    github: "https://github.com/zoseeee/mini-game",
   },
   {
     id: 5,

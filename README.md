7547c4ae8cbb49ea8947f9f7a51e0c5941848c16
jigooo <h00kk@naver.com>
2023-02-11 18:33:13 +0900

    0211

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # 2023 Portfolio [![2023 Portfolio](https://cdn.jsdelivr.net/gh/sindresorhus/awesome@d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://2023-portfolio.netlify.app/)
-> A curated list of gentle-monster READMEs
 
 <br>
 
 🔗 [배포 링크](https://2023-portfolio.netlify.app/) 
-* `react-router`를 사용하여 메인화면, 프로젝트, 웹 퍼블리싱, 모달창 등으로 구성된 포트폴리오 페이지입니다. (반응형 추가 예정입니다.)
+* `react-router`를 사용하여 메인화면, 프로젝트, 웹 퍼블리싱, 모달창 등으로 구성된 포트폴리오 페이지입니다. (반응형 수정중입니다.)
 
 <img src="./public/images/github01.png" alt=" " />
 
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import DesignPage from "./components/DesignPage/DesignPage";
 import Header from "./components/Header/Header";
+import LoadingPage from "./components/LoadingPage/LoadingPage";
 import Main from "./components/Main/Main";
 import ProjectPage from "./components/ProjectPage/ProjectPage";
 import "./css/app.scss";
@@ -11,6 +12,7 @@ import { RD, DB } from "./data";
 const App = () => {
   const [reactItems, setReactItems] = useState([]);
   const [designItems, setDesignItems] = useState([]);
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     setReactItems(RD);
@@ -18,6 +20,29 @@ const App = () => {
   useEffect(() => {
     setDesignItems(DB);
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 6000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  // LoadingPage 포함
+  return load === true ? (
+    <LoadingPage />
+  ) : (
+    <div className="Wrapper">
+      <Header RD={reactItems} DB={designItems} />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/project" element={<ProjectPage RD={reactItems} />} />
+        <Route path="/javascript" element={<DesignPage DB={designItems} />} />
+      </Routes>
+    </div>
+  );
 
   return (
     <div className="Wrapper">

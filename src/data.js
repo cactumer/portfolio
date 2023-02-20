45c451eb3bfecb730f6a33062782acf233c3f170
jigooo <h00kk@naver.com>
2023-02-20 11:54:08 +0900

    � data.js 수정

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const RD = [
     tools:
       " react / react-router-dom / react-query / scss / firebase / cloudinary / netlify",
     url: "https://delicate-longma-d20402.netlify.app/",
-    github: "https://github.com/zoseeee/gentle-monster",
+    github: "https://github.com/cactumer/gentle-monster",
   },
   {
     id: 1,
@@ -16,7 +16,7 @@ const RD = [
       "본 프로젝트는 React를 이용하여 홈, 공식상영작, 수상작 및 수상자, 작품 검색 등을 구현한 페이지입니다. Axios로 영화진흥위원회 Api를 호출하여 수상자 및 수상작 페이지를 구성하였고 OMDb Api를 이용하여 영화 검색이 가능하도록 구현하였습니다. Netlify로 배포 후 수정하고 있습니다.",
     tools: " react / react-router-dom / scss / open api / axios / netlify ",
     url: "https://classy-madeleine-f766d7.netlify.app/",
-    github: "https://github.com/zoseeee/biff-react",
+    github: "https://github.com/cactumer/biff-react",
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ const RD = [
       "본 프로젝트는 React를 이용하여, 홈, 로그인, 카드 편집, 등록, 미리보기 등으로 구성되어있습니다. 최근 재밌게 보았던 영화 ‘everything everywhere all at once’ 를 테마로 만든 프로젝트입니다. Firebase를 통해 로그인이 가능하도록 하였고 사용자의 정보를 Firebase의 Realtime Database와 Cloudinary에서 실시간 등록 및 수정이 가능하도록 하였습니다.",
     tools: " react / react-router-dom / css / firebase / cloudinary / netlify ",
     url: "https://rococo-lamington-8b84b3.netlify.app/",
-    github: "https://github.com/zoseeee/Card-Maker",
+    github: "https://github.com/cactumer/Card-Maker",
   },
   {
     id: 3,
@@ -33,8 +33,8 @@ const RD = [
     description:
       "본 프로젝트는 React를 이용하여 인기 동영상, 검색, 동영상 재생 등의 기능을 담은 YouTube 클론 코딩 페이지입니다. Youtube Api를 사용하여 원하는 영상을 검색할 수 있으며, 현재 대한민국 인기 동영상 20개의 리스트를 얻어와 메인화면에 구현하였습니다",
     tools: " react / css / axios / youtube api ",
-    url: "https://zoseeee.github.io/YouTube/",
-    github: "https://github.com/zoseeee/YouTube",
+    url: "https://cactumer.github.io/YouTube/",
+    github: "https://github.com/cactumer/YouTube",
   },
   {
     id: 4,
@@ -42,8 +42,8 @@ const RD = [
     description:
       "본 프로젝트는 Javascript로 만든 미니 게임입니다. 영화 <Harry Potter> 속 퀴티치 경기를 테마로 만들었습니다. 15초의 시간이 주어지고 시간 내에 골든 스니치 10개를 클릭하면 성공하는 아주 간단한 게임입니다. ",
     tools: " javascript / css  ",
-    url: "https://zoseeee.github.io/mini-game/",
-    github: "https://github.com/zoseeee/mini-game",
+    url: "https://cactumer.github.io/mini-game/",
+    github: "https://github.com/cactumer/mini-game",
   },
   {
     id: 5,
@@ -52,7 +52,7 @@ const RD = [
       "본 프로젝트는 React를 이용하여 메인화면, 프로젝트, 웹 퍼블리싱, 모달창 등으로 구성된 포트폴리오 페이지입니다. Netlify로 배포 후 수정하고 있습니다. (각각의 프로젝트의 부족한 부분들은 계속해서 수정중입니다... 감사합니다.)",
     tools: "react / react-router-dom / scss / netlify",
     url: "https://2023-portfolio.netlify.app/",
-    github: "https://github.com/zoseeee/portfolio",
+    github: "https://github.com/cactumer/portfolio",
   },
 ];
 
@@ -60,44 +60,44 @@ const DB = [
   {
     id: 0,
     title: "jasaeng",
-    url: "https://zoseeee.github.io/jaseng/",
-    github: "https://github.com/zoseeee/jaseng",
+    url: "https://cactumer.github.io/jaseng/",
+    github: "https://github.com/cactumer/jaseng",
   },
   {
     id: 1,
     title: "kidZania",
-    url: "https://zoseeee.github.io/KidZania/",
-    github: "https://github.com/zoseeee/KidZania",
+    url: "https://cactumer.github.io/KidZania/",
+    github: "https://github.com/cactumer/KidZania",
   },
   {
     id: 2,
     title: "hince",
-    url: "https://zoseeee.github.io/hince/",
-    github: "https://github.com/zoseeee/hince",
+    url: "https://cactumer.github.io/hince/",
+    github: "https://github.com/cactumer/hince",
   },
   {
     id: 3,
     title: "gopizza",
-    url: "https://zoseeee.github.io/GOPIZZA/",
-    github: "https://github.com/zoseeee/GOPIZZA",
+    url: "https://cactumer.github.io/GOPIZZA/",
+    github: "https://github.com/cactumer/GOPIZZA",
   },
   {
     id: 4,
     title: "haagen-dazs",
-    url: "https://zoseeee.github.io/haagendazs/",
-    github: "https://github.com/zoseeee/haagendazs",
+    url: "https://cactumer.github.io/haagendazs/",
+    github: "https://github.com/cactumer/haagendazs",
   },
   {
     id: 5,
     title: "jakomo",
-    url: "https://zoseeee.github.io/jakomo/",
-    github: "https://github.com/zoseeee/jakomo",
+    url: "https://cactumer.github.io/jakomo/",
+    github: "https://github.com/cactumer/jakomo",
   },
   {
     id: 6,
     title: "hanwha",
-    url: "https://zoseeee.github.io/hanwha/",
-    github: "https://github.com/zoseeee/hanwha",
+    url: "https://cactumer.github.io/hanwha/",
+    github: "https://github.com/cactumer/hanwha",
   },
 ];
 
